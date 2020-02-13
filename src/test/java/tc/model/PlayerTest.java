a48d9cc78dc68eeeb91e1421f214997bbf0db3d6
fettqa <fettqa@gmail.com>
2020-02-13 21:50:54 +0300

    TestPlayer added

--- src/test/java/tc/model/PlayerTest.java
+++ src/test/java/tc/model/PlayerTest.java
@@ -6,9 +6,19 @@ class PlayerTest {
 
     @org.junit.jupiter.api.Test
     void getName() {
+        final String testName = "FirstPlayer";
+        final String expectedName = testName;
+        final Player player = new Player(testName, null);
+        final String actualName = player.getName();
+        assertEquals(expectedName,actualName);
     }
 
     @org.junit.jupiter.api.Test
     void getFigure() {
+        final Figure testFigure = Figure.P1;
+        final Figure expectedFigure = testFigure;
+        final Player player = new Player(null, testFigure);
+        final Figure actualFigure = player.getFigure();
+        assertEquals(expectedFigure,actualFigure);
     }
 }
