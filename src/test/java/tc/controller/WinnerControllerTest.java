0612052fb977af19e83adc9cbfbe1b2c7ce85e89
fettqa <fettqa@gmail.com>
2020-02-16 02:31:14 +0300

    WinnerControllerTest created. tests for win conditions added.

---
+++ src/test/java/tc/controller/WinnerControllerTest.java
@@ -0,0 +1,47 @@
+package tc.controller;
+
+import org.junit.jupiter.api.Test;
+import tc.model.Direction;
+import tc.model.Field;
+import tc.model.Figure;
+import tc.model.exceptions.AlreadyOccupiedException;
+import tc.model.exceptions.InvalidMoveDirection;
+import tc.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinnerControllerTest {
+
+    @Test
+    void getWinnerWhenSecondPlayerCanNotMove() throws InvalidPointException, AlreadyOccupiedException, InvalidMoveDirection {
+        Field field = new Field();
+        MoveController moveController = new MoveController();
+        WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 2; i++){
+            moveController.moveFigure(field, Figure.SecondPlayer.FIGURE, Direction.UP);
+            moveController.moveFigure(field, Figure.FirstPlayer.FIRST_FIGURE, Direction.UP);
+            moveController.moveFigure(field, Figure.FirstPlayer.SECOND_FIGURE, Direction.UP);
+            moveController.moveFigure(field, Figure.FirstPlayer.THIRD_FIGURE, Direction.UP);
+        }
+        moveController.moveFigure(field, Figure.FirstPlayer.SECOND_FIGURE, Direction.UP);
+        assertFalse(winnerController.winnerIsSecondPlayer(field));
+        assertEquals(Figure.SecondPlayer.FIGURE,field.getFigure(new Point(1,0)));
+    }
+
+    @Test
+    void getWinnerWhenSecondPlayerBelowThanFirstPlayer() throws InvalidPointException, AlreadyOccupiedException, InvalidMoveDirection {
+        Field field = new Field();
+        MoveController moveController = new MoveController();
+        WinnerController winnerController = new WinnerController();
+        moveController.moveFigure(field, Figure.FirstPlayer.FIRST_FIGURE, Direction.UP);
+        moveController.moveFigure(field, Figure.FirstPlayer.FIRST_FIGURE, Direction.UP);
+        moveController.moveFigure(field, Figure.FirstPlayer.SECOND_FIGURE, Direction.UP);
+        moveController.moveFigure(field, Figure.FirstPlayer.THIRD_FIGURE, Direction.UP);
+        moveController.moveFigure(field, Figure.FirstPlayer.SECOND_FIGURE, Direction.LEFT);
+        moveController.moveFigure(field, Figure.FirstPlayer.SECOND_FIGURE, Direction.UP);
+        moveController.moveFigure(field, Figure.SecondPlayer.FIGURE, Direction.DOWN);
+        assertTrue(winnerController.winnerIsSecondPlayer(field));
+    }
+}
