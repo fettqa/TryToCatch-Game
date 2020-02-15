c6ced615a697fc32540ce7d88cf0484513040c5a
fettqa <fettqa@gmail.com>
2020-02-16 01:34:38 +0300

    WinnerController updated. fix winner conditions.

--- src/main/java/tc/controller/WinnerController.java
+++ src/main/java/tc/controller/WinnerController.java
@@ -1,15 +1,42 @@
 package tc.controller;
 
+import tc.model.Direction;
 import tc.model.Field;
 import tc.model.Figure;
+import tc.model.exceptions.AlreadyOccupiedException;
+import tc.model.exceptions.InvalidPointException;
+
+import java.awt.*;
 
 public class WinnerController {
 
-    public Figure getWinner(Field field, Figure player) {
-
-        if (Figure.secondPlayer.FIGURE.equals(player)){
-        return null;
+    public boolean winnerIsSecondPlayer(Field field) {
+        Figure PlayerTwoFigure = Figure.SecondPlayer.FIGURE;
+        if(MoveController.firstPlayerLocationOf1stFigure.y < MoveController.secondPlayerLocation.y ||
+        MoveController.firstPlayerLocationOf2ndFigure.y < MoveController.secondPlayerLocation.y ||
+        MoveController.firstPlayerLocationOf3rdFigure.y < MoveController.secondPlayerLocation.y) {
+            return true;
         }
-        return null;
+        try{
+            field.setFigure(new Point(MoveController.secondPlayerLocation.x,MoveController.secondPlayerLocation.y + 1),PlayerTwoFigure,Direction.DOWN);
+            return true;
+        } catch (AlreadyOccupiedException | InvalidPointException e){
+            try {
+                field.setFigure(new Point(MoveController.secondPlayerLocation.x,MoveController.secondPlayerLocation.y - 1),PlayerTwoFigure,Direction.UP);
+                return true;
+            } catch (AlreadyOccupiedException | InvalidPointException r) {
+                try {
+                    field.setFigure(new Point(MoveController.secondPlayerLocation.x + 1,MoveController.secondPlayerLocation.y),PlayerTwoFigure, Direction.RIGHT);
+                    return true;
+                }catch (AlreadyOccupiedException | InvalidPointException t) {
+                    try{
+                        field.setFigure(new Point(MoveController.secondPlayerLocation.x - 1,MoveController.secondPlayerLocation.y),PlayerTwoFigure, Direction.LEFT);
+                        return true;
+                    }catch (AlreadyOccupiedException | InvalidPointException y){
+                        return false;
+                    }
+                }
+            }
+        }
     }
 }
