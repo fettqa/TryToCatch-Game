d0da532118e643968ccc1cc558a56eb1262f5aef
fettqa <fettqa@gmail.com>
2020-02-16 01:35:15 +0300

    MoveController save points is fixed

--- src/main/java/tc/controller/MoveController.java
+++ src/main/java/tc/controller/MoveController.java
@@ -26,22 +26,22 @@ public class MoveController {
                                                                 AlreadyOccupiedException,
                                                                 InvalidPointException {
         if(Direction.DOWN.equals(direction)){
-            if (Figure.firstPlayer.FIRST_FIGURE.equals(figure) ||
-                    Figure.firstPlayer.SECOND_FIGURE.equals(figure) ||
-                    Figure.firstPlayer.THIRD_FIGURE.equals(figure)) {
+            if (Figure.FirstPlayer.FIRST_FIGURE.equals(figure) ||
+                    Figure.FirstPlayer.SECOND_FIGURE.equals(figure) ||
+                    Figure.FirstPlayer.THIRD_FIGURE.equals(figure)) {
                 System.out.println("Игрок 2 не может делать ход вниз");
                 throw new InvalidMoveDirection();
             }
-            playerTurn(field,figure,point,p -> new Point(p.x,p.y + 1));
+            playerTurn(field,figure,point,p -> new Point(p.x,p.y + 1),direction);
         }
         if(Direction.UP.equals(direction)){
-            playerTurn(field,figure,point,p -> new Point(p.x,p.y - 1));
+            playerTurn(field,figure,point,p -> new Point(p.x,p.y - 1),direction);
         }
         if(Direction.LEFT.equals(direction)){
-            playerTurn(field,figure,point,p -> new Point(p.x - 1,p.y));
+            playerTurn(field,figure,point,p -> new Point(p.x - 1,p.y),direction);
         }
         if(Direction.RIGHT.equals(direction)){
-            playerTurn(field,figure,point,p -> new Point(p.x + 1,p.y));
+            playerTurn(field,figure,point,p -> new Point(p.x + 1,p.y),direction);
         }
     }
 
@@ -51,19 +51,20 @@ public class MoveController {
             AlreadyOccupiedException,
             InvalidPointException{
         Point point = new Point();
-        if(Figure.firstPlayer.FIRST_FIGURE.equals(figure)) point = MoveController.firstPlayerLocationOf1stFigure;
-        if(Figure.firstPlayer.SECOND_FIGURE.equals(figure)) point = MoveController.firstPlayerLocationOf2ndFigure;
-        if(Figure.firstPlayer.THIRD_FIGURE.equals(figure)) point = MoveController.firstPlayerLocationOf3rdFigure;
-        if(Figure.secondPlayer.FIGURE.equals(figure)) point = MoveController.secondPlayerLocation;
+        if(Figure.FirstPlayer.FIRST_FIGURE.equals(figure)) point = firstPlayerLocationOf1stFigure;
+        if(Figure.FirstPlayer.SECOND_FIGURE.equals(figure)) point = firstPlayerLocationOf2ndFigure;
+        if(Figure.FirstPlayer.THIRD_FIGURE.equals(figure)) point = firstPlayerLocationOf3rdFigure;
+        if(Figure.SecondPlayer.FIGURE.equals(figure)) point = secondPlayerLocation;
         moveFigure(field,figure,point,direction);
     }
 
     private void playerTurn(final Field field,
                            final Figure figure,
                            final Point point,
-                           final IDirectionPoint directionPoint) throws AlreadyOccupiedException, InvalidPointException {
+                           final IDirectionPoint directionPoint,
+                            final Direction direction) throws AlreadyOccupiedException, InvalidPointException {
             final Point nextPoint = directionPoint.next(point);
-            field.setFigure(nextPoint, figure);
+            field.setFigure(nextPoint, figure, direction);
             saveFigureLocation(figure, nextPoint);
             field.deleteFigure(point);
     }
@@ -73,24 +74,9 @@ public class MoveController {
     }
 
     private void saveFigureLocation(final Figure figure, final Point point) {
-        if(Figure.secondPlayer.FIGURE.equals(figure)) {
-            secondPlayerLocation.y = point.y;
-            secondPlayerLocation.x = point.x;
-        }
-
-        if(Figure.firstPlayer.FIRST_FIGURE.equals(figure)) {
-            firstPlayerLocationOf1stFigure.y = point.y;
-            firstPlayerLocationOf1stFigure.x = point.x;
-        }
-
-        if(Figure.firstPlayer.SECOND_FIGURE.equals(figure)) {
-            firstPlayerLocationOf2ndFigure.y = point.y;
-            firstPlayerLocationOf2ndFigure.x = point.x;
-        }
-
-        if(Figure.firstPlayer.THIRD_FIGURE.equals(figure)) {
-            firstPlayerLocationOf3rdFigure.y = point.y;
-            firstPlayerLocationOf3rdFigure.x = point.x;
-        }
+        if(Figure.FirstPlayer.FIRST_FIGURE.equals(figure)) firstPlayerLocationOf1stFigure = point;
+        if(Figure.FirstPlayer.SECOND_FIGURE.equals(figure)) firstPlayerLocationOf2ndFigure = point;
+        if(Figure.FirstPlayer.THIRD_FIGURE.equals(figure)) firstPlayerLocationOf3rdFigure = point;
+        if(Figure.SecondPlayer.FIGURE.equals(figure)) secondPlayerLocation = point;
     }
 }
