37b8570fa50ce1d3af70f03eda9c67e392dd5b0a
fettqa <fettqa@gmail.com>
2020-02-16 04:26:09 +0300

    Updated Enum of figures. Added getters.

--- src/main/java/tc/model/Figure.java
+++ src/main/java/tc/model/Figure.java
@@ -2,23 +2,51 @@ package tc.model;
 
 public interface Figure {
 
+    String getFigure();
+
     enum FirstPlayer implements Figure {
-        FIRST_FIGURE("W"),
-        SECOND_FIGURE("V"),
-        THIRD_FIGURE("M");
+        FIRST_FIGURE("[W]"),
+        SECOND_FIGURE("[V]"),
+        THIRD_FIGURE("[M]");
+
+        String firstPlayerFigure;
 
         FirstPlayer(String w) {
+            firstPlayerFigure = w;
+        }
+
+        @Override
+        public String getFigure(){
+            return firstPlayerFigure;
         }
     }
     enum SecondPlayer implements Figure {
-        FIGURE("*");
-        SecondPlayer(String w) {
+        FIGURE("[*]");
+
+        String secondPlayerFigure;
+
+        SecondPlayer(String s) {
+            secondPlayerFigure = s;
+        }
+
+        @Override
+        public String getFigure() {
+            return secondPlayerFigure;
         }
     }
 
     enum AngleOfField implements Figure {
-        ANGLE(" ");
-        AngleOfField(String s) {
+        ANGLE("   ");
+
+        String angle;
+
+        AngleOfField(String d) {
+            angle = d;
+        }
+
+        @Override
+        public String getFigure() {
+            return angle;
         }
     }
 }
